e4e7b7b1f0f52e41e586b5b9093b930331772aad
Arian <115090317+arianoso@users.noreply.github.com>
2023-01-31 16:19:33 -0600

    add wifi6

---
+++ wifi6/js/sweetalert.js
@@ -0,0 +1,14 @@
+$("#QueWifi6").click(function(){
+	Swal.fire({
+		title: '¿Que es Wifi 6?',
+		html: 'Wi-Fi 6 es una mejora importante con respecto a las generaciones anteriores, aunque a primera vista, las diferencias pueden no ser obvias para el usuario promedio. Estos cambios pueden no alterar de forma drástica el modo en que usamos routers o redes inalámbricos, sino que consisten en muchas mejoras que, combinadas, dan como resultado una actualización sustancial. <br> <br> El primer gran cambio es que Wi-Fi 6 permite velocidades de conexión potencialmente más rápidas.',
+		footer: '<a href="/">Jailbreak-Update</a>',
+		showCloseButton: true,
+		showCancelButton: true,
+		confirmButtonText: '<a href="/wifi6/que-es-wifi6.html">Saber mas</a>',
+		cancelButtonText: 'Cerrar'
+	  })
+})
+
+
+
